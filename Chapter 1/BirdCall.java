1fbcb5b1ab9cfa7bb2cbffc8950fefa7bf67702b
JLJL0308 <jcliwork@gmail.com>
2023-05-10 22:25:42 +0100

    Add files.

---
+++ Chapter 1/BirdCall.java
@@ -0,0 +1,9 @@
+import packagea.Bird;
+
+public class BirdCall {
+	
+    public static void main(String[] args) {
+       Bird bird = new Bird();
+    }
+    
+}
---
+++ Chapter 1/packagea/Bird.java
@@ -0,0 +1,8 @@
+package packagea;
+
+public class Bird {
+    public static void main(String[] args) {
+       { System.out.println("Feathers"); }
+    }
+    {System.out.println("Snowy");} 
+}
